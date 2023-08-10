9adeb70c51b968e04f7eab3b058860b36a962429
Theideabased <ogunmusireseyi01@gmail.com>
2023-08-10 15:47:26 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,13 @@ import uuid
 from datetime import datetime
 import json
 
+
 class BaseModel:
     """ The base of all other model """
 
     def __init__(self, id, created_at, updated_at):
         """
-        id is generated woth the uuid random unique 
+        id is generated woth the uuid random unique
         value generatir and it is converted to a string
         created_at is the date of creation.
         updated_at is the date of each modification.
@@ -20,13 +21,13 @@ class BaseModel:
         created_at = datetime.now()
         self.created_at = created_at
         self.updated_at = created_at
-   
-   def __str__(self):
-       """
-       this will print out the string of 
-       [<class name>] (<self.id>) <self.__dict__>
-       """
-       print(f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
+
+    def __str__(self):
+        """
+        this will print out the string of
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        print(f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
 
     def save(self):
         """
@@ -42,6 +43,7 @@ class BaseModel:
         self.created_at = created_at.isoformat()
         self.updated_at = updated_at.isoformat()
         obj_string = f'{ "name":{self.__class__}, "id":{self.id}, \
-                "created_at":{self.created_at}, "updated_at":{self.updated_at}}'
+                "created_at":{self.created_at}, \
+                "updated_at":{self.updated_at}}'
         self.__dict__ = json.loads(obj_string)
         return self.__dict__
