416854b218bcd0466ad557d123522abc241cc3a8
Theideabased <ogunmusireseyi01@gmail.com>
2023-08-12 12:04:20 -0700

    base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This will test all the method and attributes of base_model """
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    """ this will test all the instance and 
+    methods in BaseModel in different style """
+    def test_id(self):
+        """
+        to test if the id is a string
+        """
+
