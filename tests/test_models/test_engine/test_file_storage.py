a423575f435ff37ed86d80e5498db9dd4de7a724
Omar <omar0642624692@gmail.com>
2023-08-13 16:46:28 -0700

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
+"""---"""
 import unittest
-import pep8
 import json
 import os
 from models.base_model import BaseModel
@@ -11,40 +10,78 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
-""" pep8 is now pycodestyle """
-
-
-class TestFileStorageDocs(unittest.TestCase):
-    """ check for documentation """
-    def test_class_doc(self):
-        """ check for class documentation """
-        self.assertTrue(len(FileStorage.__doc__) > 0)
-
-
-class TestFileStoragePep8(unittest.TestCase):
-    """ check for pep8 validation """
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/engine/file_storage.py'
-        file2 = 'tests/test_models/test_engine/test_file_storage.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
-
 
 class TestFileStorage(unittest.TestCase):
-    """ tests for class FileStorage """
-    @classmethod
-    def setUpClass(cls):
-        """ set up instances for all tests """
-        storage = FileStorage()
+    """---"""
+
+    def setUp(self):
+        """---"""
+        self.file_path = "test_file.json"
+        self.storage = FileStorage()
+        self.storage._FileStorage__file_path = self.file_path
+        self.base_model = BaseModel()
+        self.user_model = User()
+        self.state_model = State()
+        self.city_model = City()
+        self.amenity_model = Amenity()
+        self.place_model = Place()
+        self.review_model = Review()
+
+        self.base_model.id = "test_base_id"
+        self.user_model.id = "test_user_id"
+        self.state_model.id = "test_state_id"
+        self.city_model.id = "test_city_id"
+        self.amenity_model.id = "test_amenity_id"
+        self.place_model.id = "test_place_id"
+        self.review_model.id = "test_review_id"
+
+        self.models = [self.base_model, self.user_model, self.state_model,
+                       self.city_model, self.amenity_model, self.place_model, self.review_model]
+
+        self.storage._FileStorage__objects = {
+            "BaseModel.test_base_id": self.base_model,
+            "User.test_user_id": self.user_model,
+            "State.test_state_id": self.state_model,
+            "City.test_city_id": self.city_model,
+            "Amenity.test_amenity_id": self.amenity_model,
+            "Place.test_place_id": self.place_model,
+            "Review.test_review_id": self.review_model
+        }
+
+    def tearDown(self):
+        """---"""
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
     def test_all(self):
-        """ test all method """
-        pass
+        """---"""
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        for model in self.models:
+            key = f"{model.__class__.__name__}.{model.id}"
+            self.assertIn(key, objects)
+            self.assertEqual(objects[key], model)
 
-    @classmethod
-    def tearDownClass(cls):
-        """ remove test instances """
-        pass
+    def test_new(self):
+        """---"""
+        new_model = BaseModel()
+        self.storage.new(new_model)
+        objects = self.storage.all()
+        key = f"{new_model.__class__.__name__}.{new_model.id}"
+        self.assertIn(key, objects)
+        self.assertEqual(objects[key], new_model)
+
+    def test_save_reload(self):
+        """---"""
+        self.storage.save()
+        self.storage._FileStorage__objects = {}
+        self.storage.reload()
+        objects = self.storage.all()
+        for model in self.models:
+            key = f"{model.__class__.__name__}.{model.id}"
+            self.assertIn(key, objects)
+            self.assertIsInstance(objects[key], model.__class__)
+            self.assertEqual(objects[key].to_dict(), model.to_dict())
+
+if __name__ == '__main__':
+    unittest.main()
