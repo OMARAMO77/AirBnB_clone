9b70f91b922157af1ad99f4e274188fc8d5daef2
Omar <omar0642624692@gmail.com>
2023-08-13 16:15:50 -0700

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,63 +1,35 @@
 #!/usr/bin/python3
+"""---"""
 import unittest
-import pep8
-import json
-import os
-from datetime import datetime
 from models.base_model import BaseModel
-""" pep8 is now change to pycodestyle """
-
-
-class TestBaseModelDocs(unittest.TestCase):
-    """ check for documentation """
-    def test_class_doc(self):
-        """ check for class documentation """
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-
-    def test_method_docs(self):
-        """ check for method documentation """
-        for func in dir(BaseModel):
-            self.assertTrue(len(func.__doc__) > 0)
-
-
-class TestBaseModelPep8(unittest.TestCase):
-    """ check for pep8 validation """
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/base_model.py'
-        file2 = 'tests/test_models/test_base_model.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
 
 
 class TestBaseModel(unittest.TestCase):
-    """ tests for class BaseModel """
-    @classmethod
-    def setUpClass(cls):
-        """ set up instances for all tests """
-        cls.basemodel = BaseModel()
+    """---"""
 
-    def test_id(self):
-        """ test id """
-        self.assertEqual(str, type(self.basemodel.id))
+    def test_init(self):
+        """---"""
+        base_model = BaseModel()
+        self.assertIsNotNone(base_model.id)
+        self.assertIsNotNone(base_model.created_at)
+        self.assertIsNotNone(base_model.updated_at)
 
-    def test_created_at(self):
-        """ test created_at """
-        self.assertEqual(datetime, type(self.basemodel.created_at))
-
-    def test_updated_at(self):
-        """ test updated_at """
-        self.assertEqual(datetime, type(self.basemodel.updated_at))
+    def test_save(self):
+        """---"""
+        base_model = BaseModel()
+        initial_updated_at = base_model.updated_at
+        base_model.save()
+        self.assertNotEqual(initial_updated_at, base_model.updated_at)
 
     def test_to_dict(self):
-        """ test to_dict method """
-        new_dict = self.basemodel.to_dict()
-        self.assertEqual(type(new_dict), dict)
-        self.assertTrue('to_dict' in dir(self.basemodel))
+        """---"""
+        base_model = BaseModel()
+        obj_dict = base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertIn('__class__', obj_dict)
+        self.assertIn('created_at', obj_dict)
+        self.assertIn('updated_at', obj_dict)
 
-    @classmethod
-    def tearDownClass(cls):
-        """ remove test instances """
-        pass
+
+if __name__ == '__main__':
+    unittest.main()
