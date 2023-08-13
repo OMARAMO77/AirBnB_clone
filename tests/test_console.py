b7a9dbf2825bee12b520da3bab005b49e3bab223
Omar <omar0642624692@gmail.com>
2023-08-13 16:40:18 -0700

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,39 +1,38 @@
 #!/usr/bin/python3
-import unittest
-import pep8
-import json
-import os
-from datetime import datetime
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
+"""---"""
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
-from models.engine.file_storage import FileStorage
-from console import HBNBCommand
+from models.state import State
+import unittest
 
 
-class TestHBNBCommandDocs(unittest.TestCase):
-    """ check for documentation """
-    def test_class_doc(self):
-        """ check for class documentation """
-        self.assertTrue(len(HBNBCommand.__doc__) > 0)
+class TestConsole(unittest.TestCase):
+    """---"""
+    def test_class(self):
+        """---"""
+        city_1 = City()
+        amenity_1 = Amenity()
+        state_1 = State()
+        review_1 = Review()
+        place_1 = Place()
+        self.assertEqual(city_1.__class__.__name__, "City")
+        self.assertEqual(amenity_1.__class__.__name__, "Amenity")
+        self.assertEqual(state_1.__class__.__name__, "State")
+        self.assertEqual(review_1.__class__.__name__, "Review")
+        self.assertEqual(place_1.__class__.__name__, "Place")
 
-
-class TestHBNBCommandPep8(unittest.TestCase):
-    """ check for pep8 validation """
-    def test_pep8(self):
-        """ test base and test_base for pep8(pycodestyle) conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'console.py'
-        file2 = 'tests/test_console.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
-
-
-class TestHBNBCommand(unittest.TestCase):
-    """ tests for class HBNBCommand """
-    pass
+    def test_father(self):
+        """---"""
+        city_1 = City()
+        amenity_1 = Amenity()
+        state_1 = State()
+        review_1 = Review()
+        place_1 = Place()
+        self.assertTrue(issubclass(city_1.__class__, BaseModel))
+        self.assertTrue(issubclass(amenity_1.__class__, BaseModel))
+        self.assertTrue(issubclass(state_1.__class__, BaseModel))
+        self.assertTrue(issubclass(review_1.__class__, BaseModel))
+        self.assertTrue(issubclass(place_1.__class__, BaseModel))
