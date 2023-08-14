ca1648aa62ccded74c25df77dea14d49f5cc3dac
Omar <omar0642624692@gmail.com>
2023-08-13 18:06:47 -0700

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,38 +1,164 @@
 #!/usr/bin/python3
-"""---"""
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+from models.user import User
+
+class TestHBNBCommand(unittest.TestCase):
+
+    def setUp(self):
+        self.hbnb = HBNBCommand()
+
+    def tearDown(self):
+        self.hbnb = None
+
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_show(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create BaseModel')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show BaseModel {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[BaseModel]" in output and instance_id in output)
+
+    def test_create_user(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create User")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_create_state(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create State")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_create_city(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create City")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_create_amenity(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create Amenity")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_create_place(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create Place")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_create_review(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd("create Review")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)
+
+    def test_show_user(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create User')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show User {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[User]" in output and instance_id in output)
+
+    def test_show_state(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create State')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show State {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[State]" in output and instance_id in output)
+
+    def test_show_city(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create City')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show City {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[City]" in output and instance_id in output)
+
+    def test_show_amenity(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Amenity')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show Amenity {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[Amenity]" in output and instance_id in output)
+
+    def test_show_place(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Place')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show Place {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[Place]" in output and instance_id in output)
+
+    def test_show_review(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Review')
+            instance_id = f.getvalue().strip()
+            self.hbnb.onecmd(f'show Review {instance_id}')
+            output = f.getvalue().strip()
+            self.assertTrue("[Review]" in output and instance_id in output)
+
+    def test_all_user(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create User')
+            self.hbnb.onecmd('create User')
+            self.hbnb.onecmd('all User')
+            output = f.getvalue().strip()
+            self.assertTrue("[User]" in output)
+
+    def test_all_state(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create State')
+            self.hbnb.onecmd('create State')
+            self.hbnb.onecmd('all State')
+            output = f.getvalue().strip()
+            self.assertTrue("[State]" in output)
+
+    def test_all_city(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create City')
+            self.hbnb.onecmd('create City')
+            self.hbnb.onecmd('all City')
+            output = f.getvalue().strip()
+            self.assertTrue("[City]" in output)
+
+    def test_all_amenity(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Amenity')
+            self.hbnb.onecmd('create Amenity')
+            self.hbnb.onecmd('all Amenity')
+            output = f.getvalue().strip()
+            self.assertTrue("[Amenity]" in output)
+
+    def test_all_place(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Place')
+            self.hbnb.onecmd('create Place')
+            self.hbnb.onecmd('all Place')
+            output = f.getvalue().strip()
+            self.assertTrue("[Place]" in output)
+
+    def test_all_review(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb.onecmd('create Review')
+            self.hbnb.onecmd('create Review')
+            self.hbnb.onecmd('all Review')
+            output = f.getvalue().strip()
+            self.assertTrue("[Review]" in output)
 
 
-class TestConsole(unittest.TestCase):
-    """---"""
-    def test_class(self):
-        """---"""
-        city_1 = City()
-        amenity_1 = Amenity()
-        state_1 = State()
-        review_1 = Review()
-        place_1 = Place()
-        self.assertEqual(city_1.__class__.__name__, "City")
-        self.assertEqual(amenity_1.__class__.__name__, "Amenity")
-        self.assertEqual(state_1.__class__.__name__, "State")
-        self.assertEqual(review_1.__class__.__name__, "Review")
-        self.assertEqual(place_1.__class__.__name__, "Place")
-
-    def test_father(self):
-        """---"""
-        city_1 = City()
-        amenity_1 = Amenity()
-        state_1 = State()
-        review_1 = Review()
-        place_1 = Place()
-        self.assertTrue(issubclass(city_1.__class__, BaseModel))
-        self.assertTrue(issubclass(amenity_1.__class__, BaseModel))
-        self.assertTrue(issubclass(state_1.__class__, BaseModel))
-        self.assertTrue(issubclass(review_1.__class__, BaseModel))
-        self.assertTrue(issubclass(place_1.__class__, BaseModel))
+if __name__ == '__main__':
+    unittest.main()
